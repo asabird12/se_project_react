57e6130184bad4b0e2edd5fe1d5013718384cc4c
asabird12 <160693729+asabird12@users.noreply.github.com>
2025-03-13 20:53:57 -0400

    sprint 10 submission 1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 
 import "./App.css";
-import { coordinates, APIkey } from "../Utils/constants";
+import { coordinates, APIkey } from "../../Utils/constants";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
 import ItemModal from "../ItemModal/ItemModal";
-import { filterWeatherData, getWeather } from "../Utils/weatherApi";
+import { filterWeatherData, getWeather } from "../../Utils/weatherApi";
 
 function App() {
   const [weatherData, setWeatherData] = useState({
