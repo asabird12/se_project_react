ef90126fab96b58d57f4e3d123056a4623b43c91
asabird12 <160693729+asabird12@users.noreply.github.com>
2025-03-13 21:20:30 -0400

    sprint 10 submission 1

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,7 @@
 import "./Main.css";
 import WeatherCard from "../WeatherCard/WeatherCard";
 import ItemCard from "../ItemCard/ItemCard";
-import { DefaultClothingItems } from "../../babba/constants";
+import { DefaultClothingItems } from "../../utils/constants";
 
 function Main({ weatherData, handleCardClick }) {
   return (
